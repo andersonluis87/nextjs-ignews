2a9b6db976a0ef2e3f0aa3bf1eb9f1c14e533f7e
Anderson Luis Silva <andersonluis87@gmail.com>
2023-04-26 02:14:12 -0300

    Creating barebones of signIn button

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,17 +1,38 @@
+import { useState } from 'react'
 import styles from './styles.module.scss'
 import Image from 'next/image'
+import { SignInButton } from '../SignInButton'
 
 export function Header() {
+  const [activeMenu, setActiveMenu] = useState('home')
+
+  const handleActiveMenu = (menu: string) => {
+    setActiveMenu(menu)
+  }
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="ig.news" width={110} height={31} />
+
         <nav>
-          <a href="" className={styles.active}>
+          <a
+            href="#"
+            className={activeMenu === 'home' ? styles.active : ''}
+            onClick={() => handleActiveMenu('home')}
+          >
             Home
           </a>
-          <a href="">Posts</a>
+          <a
+            href="#"
+            className={activeMenu === 'posts' ? styles.active : ''}
+            onClick={() => handleActiveMenu('posts')}
+          >
+            Posts
+          </a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
