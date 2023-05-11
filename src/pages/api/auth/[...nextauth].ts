2ddaff741d37beb9b4306bc64e83b0b069c15ff2
Anderson Luis Silva <andersonluis87@gmail.com>
2023-05-10 23:26:34 -0300

    Adding fauna.Query to validate if the user exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,44 @@
-import NextAuth from 'next-auth'
+import NextAuth, { AuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../services/fauna'
+import { query as q } from 'faunadb'
 
-export const authOptions = {
-  // Configure one or more authentication providers
+export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { name, email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email as q.ExprArg)
+                )
+              )
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email, name },
+            }),
+            q.Get(
+              q.Match(q.Index('user_by_email'), q.Casefold(email as q.ExprArg))
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    },
+  },
 }
 export default NextAuth(authOptions)
